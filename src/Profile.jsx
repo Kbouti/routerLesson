9b6db6e9ff80092eaae3a98fc7c136264524b257
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-16 15:19:23 -0700

    Utilize first router

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Profile() {
+const Profile = () => {
   return (
     <div>
       <h1>Hello from your profile page!</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App'
+import Profile from "./Profile"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  }
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router} />
   </React.StrictMode>,
 )
