daafef517aac9d8d998617bc13e02aaeace5b787
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-16 15:24:11 -0700

    Stop browswer from reloading by using Link element

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { Link } from "react-router-dom";
+
+
 function App() {
   return (
     <div>
@@ -11,7 +14,7 @@ function App() {
       <nav>
         <ul>
           <li>
-            <a href="profile">Profile page</a>
+            <Link to="profile">Profile page</Link>
           </li>
         </ul>
       </nav>
